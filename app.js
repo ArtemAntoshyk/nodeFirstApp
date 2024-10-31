306159898a7db73959bab3ea77c17d1f85271fda
artem <yaroslav.gorbatyuk.2010@gmail.com>
2024-10-31 14:12:40 +0200

    16. Alternative Syntax

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const names = require('./names');
 const sayHi = require('./utils');
+const data = require('./alternative-flavor')
 console.log(names);
 sayHi(names.john)
 sayHi(names.tom)
+console.log(data)
